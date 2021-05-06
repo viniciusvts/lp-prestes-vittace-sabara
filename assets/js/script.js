b7c5f2c656fb4a284e011e4f75d9bab0d92e1bb3
Vinicius de Santana <vinicius.vts@gmail.com>
2021-05-06 12:53:09 -0300

    feat: modal yt video

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,41 @@
+(function ssw(window, document, console, querySelector, querySelectorAll) {
+    // nav menu
+    document.addEventListener("DOMContentLoaded", DOMContentLoaded);
+    window.addEventListener("load", load);
+    /** O evento DOMContentLoaded é acionado quando todo o HTML foi
+     * completamente carregado e analisado, sem aguardar pelo CSS, imagens,
+     * e subframes para encerrar o carregamento.
+     */
+    function DOMContentLoaded(evt) {
+        console.log('DOMContentLoaded', evt);
+    }
+
+    /** O evento de carga é disparado quando toda a página é carregada,
+     * incluindo todos os recursos dependentes, como folhas de estilo e imagens.
+     */
+    function load(evt) {
+        startYT();
+    }
+    function startYT(){
+        // evento de clique dos videos
+        var embed = querySelectorAll('[data-embedvideo]');
+        for (const item of embed) {
+            item.addEventListener('click', function(){
+                var embedurl = this.dataset.embedvideo;
+                querySelector('#yt').src = embedurl;
+                querySelector('#ytmodal').classList.add('active');
+            });
+        }
+        // evento de clique do botão fechar
+        
+        var ytclose = querySelector('#ytclose');
+        ytclose.addEventListener('click', function(){
+            querySelector('#ytmodal').classList.remove('active');
+        })
+    }
+})(window, document, console, x=>document.querySelector(x), x=>document.querySelectorAll(x));
+
+// owl carousel
 $(document).ready(function(){
     $("#carrosselvideos").owlCarousel({
         loop:true,
